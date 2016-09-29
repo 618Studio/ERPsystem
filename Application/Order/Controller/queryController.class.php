53ebffb289d24bba83a82c247c12ae69209cc3fa
hongri1995930 <549476964@qq.com>
2016-09-29 16:41:11 +0800

    getDate

--- Application/Order/Controller/queryController.class.php
+++ Application/Order/Controller/queryController.class.php
@@ -4,6 +4,10 @@ use Think\Controller;
 use Order\Common;
 class queryController extends Controller {
     public function index(){
+        $this->display();
+    }
+
+    public function order(){
         $objetc = new Common\OrderDAOImpl();
         $this->ajaxReturn($objetc->getOrder());
     }
