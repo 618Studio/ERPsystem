a99e20cdd7fb3ffdb2c81d929ab81995f22d253b
ZhengChenYang <526789319@qq.com>
2016-09-29 13:30:10 +0800

    9-29-1

---
+++ Application/Order/Common/IOrderDAO.php
@@ -0,0 +1,16 @@
+<?php
+namespace Order\Common;
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2016/9/29
+ * Time: 13:08
+ */
+interface IOrderDAO{
+    //分页查询订单
+    public function queryOrder();
+    //增加订单
+    public function addOrder();
+    //分页获得订单
+    public function getOrder();
+}
---
+++ Application/Order/Common/OrderDAOImpl.php
@@ -0,0 +1,23 @@
+<?php
+namespace Order\Common;
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2016/9/29
+ * Time: 13:10
+ */
+class OrderDAOImpl implements IOrderDAO{
+    //查询订单
+    public function queryOrder(){
+
+    }
+
+    //增加订单
+    public function addOrder(){
+
+    }
+
+    public function getOrder(){
+        
+    }
+}
