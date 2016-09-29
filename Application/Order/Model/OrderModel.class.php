5019674694a13b4eaf5bb5b73a31753514fde88f
hongri1995930 <549476964@qq.com>
2016-09-29 22:06:50 +0800

    9-29-4

--- Application/Order/Model/OrderModel.class.php
+++ Application/Order/Model/OrderModel.class.php
@@ -14,10 +14,10 @@ class OrderModel extends Model
 {
     protected $_map = array(
         'oid' =>'order_id', // 把表单映射到数据表
-        'oCusName' =>'order_customerName',
-        'oCnr' =>'order_contractNr',
-        'oDelDate' =>'order_deliveryDate',
-        'oOrdDate' =>'order_orderDate',
+        'oCusName' =>'order_customer_name',
+        'oCnr' =>'order_contract_nr',
+        'oDelDate' =>'order_delivery_date',
+        'oOrdDate' =>'order_order_date',
         'oContent' =>'order_content',
         'oState' =>'order_state',
         'oOther' =>'order_other'
