6c59c7fd9d63c45cbcb46b6bd165cecea7563753
ZhengChenYang <526789319@qq.com>
2016-10-03 20:59:05 +0800

    10-3-6

---
+++ Application/Admin/Common/IGroupDAO.class.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2016/10/3
+ * Time: 20:54
+ */
+interface IGroupDAO{
+    //获取群组
+    public function get();
+    
+    //增加群组
+    public function add();
+
+    //修改群组
+    public function update();
+
+    //删除群组
+    public function delete();
+}
--- Application/Admin/Controller/GroupController.class.php
+++ Application/Admin/Controller/GroupController.class.php
@@ -5,3 +5,29 @@
  * Date: 2016/10/3
  * Time: 20:34
  */
+namespace Admin\Controller;
+use Think\Controller;
+use Admin\Common;
+class GroupController extends Controller{
+
+    public function index(){
+    
+        //需要一个数组 里面是所有的group，包括group所具有的权限
+        
+    }
+
+    //用于增加群组的接口
+    public function add(){
+
+    }
+
+    //用于更新群组的接口
+    public function update(){
+
+    }
+
+    //用于删除数组的接口
+    public function delete(){
+
+    }
+}
