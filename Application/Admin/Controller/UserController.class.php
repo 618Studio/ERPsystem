3fb8339eb9d7aedd4b06fc2236cd419c9a01853a
ZhengChenYang <526789319@qq.com>
2016-10-10 10:18:55 +0800

    10-10-1

---
+++ Application/Admin/Controller/UserController.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2016/10/10
+ * Time: 10:18
+ */
+namespace Admin\Controller;
+use Think\Controller;
+use Admin\Common;
+
+class UserController extends Controller{
+    function index(){
+        $this->display();
+    }
+}
