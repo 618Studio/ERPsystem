b2361e08d29e01b9b7772f8ab09a341fa7888230
ZhengChenYang <526789319@qq.com>
2016-10-18 18:18:50 +0800

    V2.1.6

---
+++ Application/Color/Controller/ProductionController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2016/10/17
+ * Time: 23:06
+ */
+namespace Color\Controller;
+use Think\Controller;
+class ProductionController extends Controller {
+    public function  index(){
+        $this->display();
+    }
+}
