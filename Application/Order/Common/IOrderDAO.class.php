ed34728684361b735204dc1d62b22cfe3cacf820
miaonot <miaonot@163.com>
2016-10-19 21:49:12 +0800

    change footer homepage still have some problems

--- Application/Order/Common/IOrderDAO.class.php
+++ Application/Order/Common/IOrderDAO.class.php
@@ -1,11 +1,14 @@
 <?php
-namespace Order\Common;
 /**
  * Created by PhpStorm.
  * User: lenovo
  * Date: 2016/9/29
  * Time: 13:08
  */
+
+namespace Order\Common;
+
+
 interface IOrderDAO{
     //分页查询订单
     public function queryOrder($param_array);
